67496054fb13e49bed910ed188e5d6ab30261e15
Renz <renz@Renzs-iMac.local>
2018-02-28 21:12:28 -0800

    adding buttons & labels

--- BalletQuiz/ViewController.swift
+++ BalletQuiz/ViewController.swift
@@ -9,7 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Properties
+    
+    @IBOutlet weak var question: UITextView!
+    
+    @IBOutlet weak var answer0: UIButton!
+    
+    @IBOutlet weak var answer1: UIButton!
+    
+    @IBOutlet weak var answer2: UIButton!
+    
+    @IBOutlet weak var answer3: UIButton!
+    
+    @IBOutlet weak var progress: UILabel!
+    
+    // Actions
+    
+    @IBAction func submitAnswer0(_ sender: Any) {
+    }
+    
+    @IBAction func submitAnswer1(_ sender: Any) {
+    }
+    
+    @IBAction func submitAnswer2(_ sender: Any) {
+    }
+    
+    @IBAction func submitAnswer3(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
